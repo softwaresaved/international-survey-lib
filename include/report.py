b49a8bf9472411c0ca27e16bb55e62eb53c2cd5a
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2021-03-04 20:05:11 +0000

    Use RSE_SURVEY_FIGURE_DPI to set figure DPI
    Also lower the default DPI to 150

--- include/report.py
+++ include/report.py
@@ -110,7 +110,7 @@ def table_country(country, name, data, index=True):
 
 def figure(name, plt):
     figpath = "fig/%s.png" % name
-    plt.savefig(figpath, dpi=300)
+    plt.savefig(figpath, dpi=os.environ.get('RSE_SURVEY_FIGURE_DPI', 150))
     plt.close('all')
     return {"f_" + name: "![%s](%s)" % (name, BASEURL + figpath)}
 
